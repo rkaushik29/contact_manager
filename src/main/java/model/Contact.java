29ae37221dd38cc5afe7039a44adca8df371d7a7
r3kaushi <r3kaushi@uwaterloo.ca>
2023-10-05 16:29:27 -0400

    Added constructors and methods

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,7 +15,7 @@ import jakarta.persistence.Table;
 @Table(name = "contacts")
 public class Contact {
 	
-	// Fields of the Contact Entity along with column name mappings
+	// Properties of the Contact Entity along with column name mappings
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,10 +37,79 @@ public class Contact {
     private String address;
     
     @Column(name = "date_created")
-    private LocalDateTime dateCreated;
+    private Date dateCreated;
 
-    // Inheritance: Each contact has a note object
+    // Composition: Each contact has a note object
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "note_id", referencedColumnName = "id")
     private Notes note;
+    
+    
+    // Default constructor
+    public Contact() {
+    }
+
+    // Parameterized constructor
+    public Contact(Long id, String firstName, String lastName, String email, String phoneNumber, String address) {
+    	this.id = id;
+    	this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.emailAddr = email;
+    	this.phoneNum = Long.valueOf(phoneNumber);	// Phone could be a string from the form, convert here
+    	this.address = address;
+    	this.dateCreated = new Date();
+    }
+    
+    // Getters and setters
+    public Long getId() {
+    	return this.id;
+    }
+
+    public void setId(Long id) {
+    	this.id = id;
+    }
+
+    public String getFirstName() {
+    	return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+    	this.firstName = firstName;
+    }
+
+    public String getLastName() {
+    	return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+    	this.lastName = lastName;
+    }
+
+    public String getEmail() {
+    	return this.emailAddr;
+    }
+    
+    public void setEmail(String email) {
+    	this.emailAddr = email;
+	}
+	
+	public String getPhoneNumber() {
+		return Long.toString(this.phoneNum);
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNum = Long.valueOf(phoneNumber);
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public Date getDateCreated() {
+		return this.dateCreated;
+	}
 }
